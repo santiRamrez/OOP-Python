5c0e72faaa69d9b28b7ca5813f3b3e5e1830fa54
Santiago Ramirez <santi_coding@outlook.com>
2022-08-27 20:10:13 -0400

    starting to learn python

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+# <literal> <operador> <literal>
+# <objeto> <operador> <objeto> ----> expresion
+
+#Text
+my_string = "hola"
+
+#Numeric
+my_int = 1
+my_float = 1.5
+my_complex = 3j
+
+#Sequence && iterables
+my_list = [1, 2, 3, 4, 5, 6, 7]
+my_tuple = ("apple", "banana", "cherry")
+my_dictionary = {
+  "nombre": "Pedro",
+  "apellido": "Perez",
+  "edad": 21,
+  "pasatiempos": {
+     "jugar": ["Fifa 22", "Baloncesto", "Mario Kart", "Futbol sala"],
+     "estudiar": ["Ingeniería en Informática", "recetas de ensaladas", "desarrollo personal", "ser más atractiv@"]
+  }
+}
+
+
+my_bool = False | True
+my_none = None
+
+
+#test = type(my_dictionary)
+print(my_bool)
